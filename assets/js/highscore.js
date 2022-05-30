edfb0ef7ca57f880c25694dc7968d6755690c06b
Devon <devondgray28@gmail.com>
2022-05-29 23:48:43 -0500

    java and highscore variables

---
+++ assets/js/highscore.js
@@ -0,0 +1,7 @@
+var backButtonEl = document.getElementById("back-btn");
+var clearButtonEl = document.getElementById("clear-btn");
+var storedScoreEl = document.getElementById("stored-score");
+var highScorePageEl = document.getElementById("high-score-page");
+var highScoreEl = document.getElementById("high-score");
+var scoreListEl = document.getElementById("score-list");
+var scoreEl = document.getElementById("score");
