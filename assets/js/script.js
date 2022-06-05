fffc739cc36ee73364b13cfa4bcb6ff714f3d365
Devon <devondgray28@gmail.com>
2022-06-05 13:06:20 -0500

    adding questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,3 +12,30 @@ var currentScore = 0;
 var inputEl = document.getElementById("initials");
 var submitBtnEl = document.getElementById("submit-btn");
 var scoreEl = document.getElementById("score");
+
+
+
+//questions to ask
+var questionArrEl = [
+    {
+      question: "In JavaScript, what is a block of a statement?",
+      choices: ["a. block that combines statements into a single compunt statement.", "b. Conditional block", "c. both conditional block and a single statement.", "d. block that contains a single statement."],
+      correct: "Answer: A",
+    },
+    {
+      question: "What is the meaning of '===' in JavaScript?",
+      choices: ["a. Comparison between two variables being more strict on equality.", "b. Comparison between two variables, but dont have to be exactly the same.", "c. Comparison between 3 variables.", "A way to add two exactly the same variables. "],
+      correct: "Answer: A",
+    },
+    {
+      question: "What is the debugger used for?",
+      choices: ["a. To help clean up messy code.", "b. To help find mistakes in your code.", "c. Debugger isnt used in JavaScript.", "d. Used to add more space between code sections."],
+      correct: "Answer: B",
+    },
+    {
+      question: "Is it True JavaScript and Java are the same thing?",
+      choices: ["True", "False"],
+      correct: "Answer: False"
+    }
+  ];
+  
