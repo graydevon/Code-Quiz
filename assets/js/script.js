d0d6577b750d6afa71ffe213890f993f8bfd8ca4
Devon <devondgray28@gmail.com>
2022-06-05 20:22:26 -0500

    fixing linked questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,11 @@
 var startButtonEl = document.getElementById("start-btn");
 var questionBoxEl = document.getElementById("question-box")
-var choiceButtonEl = document.getElementById("picks-btn");
+var choiceButtonEl = document.getElementById("choice-btn");
 var questionEl = document.getElementById("question");
 var timeEl = document.getElementById("time");
 var frontPageEl = document.getElementById("front-page");
 var currentQuestion = 0;
-var choiceBoxEl = document.getElementById("picks");
+var choiceBoxEl = document.getElementById("choices");
 var timerCount = 60;
 var gradeEl = document.getElementById("grade");
 var currentScore = 0;
@@ -20,22 +20,22 @@ var questionArrEl = [
     {
       question: "In JavaScript, what is a block of a statement?",
       choices: ["a. block that combines statements into a single compunt statement.", "b. Conditional block", "c. both conditional block and a single statement.", "d. block that contains a single statement."],
-      correct: "Answer: A",
+      correct: "a. block that combines statements into a single compunt statement.",
     },
     {
       question: "What is the meaning of '===' in JavaScript?",
       choices: ["a. Comparison between two variables being more strict on equality.", "b. Comparison between two variables, but dont have to be exactly the same.", "c. Comparison between 3 variables.", "A way to add two exactly the same variables. "],
-      correct: "Answer: A",
+      correct: "a. Comparison between two variables being more strict on equality.",
     },
     {
       question: "What is the debugger used for?",
       choices: ["a. To help clean up messy code.", "b. To help find mistakes in your code.", "c. Debugger isnt used in JavaScript.", "d. Used to add more space between code sections."],
-      correct: "Answer: B",
+      correct: "b. To help find mistakes in your code.",
     },
     {
-      question: "Is it True JavaScript and Java are the same thing?",
-      choices: ["True", "False"],
-      correct: "Answer: False"
+      question: "Is JavaScript and Java are the same thing?",
+      choices: ["Yes, they are the same exact langauge", "Same language that used difference terms", " No, they are completly different programming languages", "They both use the same terms so theyre the same"],
+      correct: "No, theyre completly different programming langages"
     }
   ];
   
